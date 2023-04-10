b6e9946b7ec23130d8bf54bad387ccc1afbbb39a
Jose Laurito <jigolaurito93@yahoo.com>
2023-04-10 00:03:07 -0500

    create Racer Display Table

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Navbar({ username, city }) {
-    // console.log(props);
-    // console.log(typeof props);
+    
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
